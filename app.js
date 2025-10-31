f6886717c39c5edd61f7ccdbce69760434a33666
DimkaZ <krepish1975@gmail.com>
2025-10-31 19:52:28 +0300

    feat: Добавили функциональность просмотра баланса

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict' ;
+
+let balance = 10000;
+
+while (true) {
+    const menu = `
+    Добро поаловать в банкомат!
+    ваш выбор:
+    1. Показать баланс
+    2. Снясть средства
+    3. Пополнить счет
+    4. Выход
+    `;
+
+    const choice = prompt(menu);
+
+    switch (choice) {
+        case '1':
+            alert(`Dfi ,fkfyc: ${баланс} ₽`)
+            break;
+    }
+}
